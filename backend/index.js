6b5780756674028d60ed835212f592525bc29382
sumukh6302 <137319752+sumukh6302@users.noreply.github.com>
2025-11-01 22:22:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import cors from "cors";
+import ConnectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js"
+import expenseRoute from "./routes/expense.route.js"
 
+dotenv.config({});
+
+
+const app = express();
+const PORT = 8000;
+
+ 
+
+//middleware
+app.use(express.json());
+app.use(urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:"http://localhost:5173",
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+
+//api's
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/expense",expenseRoute);
+
+app.listen(PORT,()=>{
+   ConnectDB();
+    console.log(`Server started at port ${PORT}`);
+})
