5385feeae549102ceb23f2a276ea5aaed2899d2e
sumukh6302 <137319752+sumukh6302@users.noreply.github.com>
2025-11-01 22:18:34 +0530

    Add files via upload

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    fullname:{
+        type:String,required:true
+    },email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+})
+
+export const User = mongoose.model("user",UserSchema);
